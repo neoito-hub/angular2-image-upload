5bd9c8732665f320b87b8a3659a104c86c83efe1
amalshehu <amalshehu@gmail.com>
2016-12-15 12:33:06 +0530

    Add image-upload modules

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpModule } from '@angular/http';
@@ -9,14 +9,28 @@ import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 
+import {ImageUploadModule} from "./shared/image-upload/image-upload.module";
+
+import {ImageUploadComponent} from "./shared/image-upload/image-upload.component";
+import {FileDropDirective} from "./shared/image-upload/file-drop.directive";
+import {ImageService} from "./shared/image-upload/image.service";
+
 @NgModule({
   imports: [BrowserModule, HttpModule, AppRoutingModule, AboutModule, HomeModule, SharedModule.forRoot()],
-  declarations: [AppComponent],
-  providers: [{
+  declarations: [AppComponent, ImageUploadComponent,
+  FileDropDirective],
+  providers: [ImageService,{
     provide: APP_BASE_HREF,
     useValue: '<%= APP_BASE %>'
   }],
   bootstrap: [AppComponent]
 
 })
-export class AppModule { }
+export class AppModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: AppModule
+      // providers: [ ImageService ]
+    }
+  }
+}
